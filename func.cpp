760354032fabcd0c342b7238cb96aeffa83af78e
omegapixel <bomb-shell@hotmail.com>
2015-12-08 16:48:59 -0600

    commit 0000000003
    out of 1000000000

---
+++ func.cpp
@@ -0,0 +1,7 @@
+#include "func.h"
+
+void cpp(int loops) {
+	for (int C = 0; C < loops; C++) {
+		printf("C--;\n");
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
 #include <iostream>
+
+#include "func.cpp"
 
 int main(int argc, char const *argv[]) {
 	std::cout << "cout\n";
 	printf("printf\n");
-	cpp();
+	cpp(12);
+	system("PAUSE");
 	return 0;
-}
-
-void cpp() {
-	for (int C = 0; C < 12; C++) {
-		std::cout<< "C++;\n";
-	}
 }
