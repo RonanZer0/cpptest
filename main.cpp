0f5c50ea855ad4f7e42646835d8e08eeb96fb947
omegapixel <bomb-shell@hotmail.com>
2015-12-08 01:40:54 -0600

    commit
    testing lol

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+	std::cout << "cout";
+	printf("printf");
+	return 0;
+}
+
+int test() {
+	for (int w = 0; w < 9999; w++) {
+		// do nothing, because...
+		// w stands for WASTE!
+	}
+}
